3023fc1684be8d1f5d5f22e6139c4ee94dfa6ad7
CasonWebDev <joao@cason.com.br>
2019-08-11 18:37:18 -0300

    add socket io support

--- src/server.js
+++ src/server.js
@@ -4,12 +4,29 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = [];
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://cason:cason@omnicason-49nnr.mongodb.net/omnicason?retryWrites=true&w=majority', {useNewUrlParser: true});
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
